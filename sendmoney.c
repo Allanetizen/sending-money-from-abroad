f27fde7281b6653b750eacd9974fc8aa30652d52
Allanetizen <39994438+Allanetizen@users.noreply.github.com>
2020-01-24 08:56:09 -0600

    Create sendmoney.c

---
+++ sendmoney.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+ main()
+ {
+int pin,amount,amount_received;
+	float dollar=100.27;
+	printf("Enter amount\n");
+	scanf("%d",&amount);
+	
+	
+	
+	
+    printf("Enter pin");
+	scanf("%d",&pin);
+	if(pin==1234)
+	
+	{amount_received=amount*dollar;
+	printf("amount sent to John Doe is \n%d",amount_received);
+	}
+	else
+	{printf("wrong pin!!!");}
+	
+	return 0;
+}
